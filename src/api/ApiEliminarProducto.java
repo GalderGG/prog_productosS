0956d03203282d9d634d11f95a57ad1721aefb56
Galder <Galder@DESKTOP-ALL3S8I>
2020-04-22 18:54:22 +0200

    ApiELiminarProducto

--- src/api/ApiEliminarProducto.java
+++ src/api/ApiEliminarProducto.java
@@ -1,7 +1,7 @@
-package controlador;
+package api;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,20 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import modelo.bean.Producto;
 import modelo.dao.ModeloProducto;
 
 /**
- * Servlet implementation class VerProductos
+ * Servlet implementation class ApiEliminarProducto
  */
-@WebServlet("/VerProductos")
-public class VerProductos extends HttpServlet {
+@WebServlet("/ApiEliminarProducto")
+public class ApiEliminarProducto extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public VerProductos() {
+    public ApiEliminarProducto() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,17 +31,25 @@ public class VerProductos extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		ModeloProducto modeloProducto = new ModeloProducto();
-		ArrayList<Producto> productos = modeloProducto.getAll();
+		int id=Integer.parseInt(request.getParameter("id"));
 		
-		request.setAttribute("productos", productos);
-		request.getRequestDispatcher("VerProductos.jsp").forward(request, response);
+		try {
+			ModeloProducto modeloProducto=new ModeloProducto();
+			modeloProducto.delete(id);
+			modeloProducto.getConexion().close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		
+		response.setHeader("Access-Control-Allow-Origin", "*"); 
+		
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
