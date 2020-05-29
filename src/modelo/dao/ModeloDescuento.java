152d10ecd7486beaeab5d96e500b30654f7fac81
Galder <Galder@DESKTOP-ALL3S8I>
2020-05-29 21:59:59 +0200

    ModeloDescuento aldaketa bisualak

--- src/modelo/dao/ModeloDescuento.java
+++ src/modelo/dao/ModeloDescuento.java
@@ -1,14 +1,48 @@
 package modelo.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import modelo.Conector;
 import modelo.bean.Descuento;
 
-public class ModeloDescuento {
-	
-	public ArrayList<Descuento> getAll(){
-		//TODO implementar metodo que devuelva todos los estados
-		return null;
+/**
+ * @author Galder
+ * @version 24.04.2020
+ *
+ */
+public class ModeloDescuento extends Conector {
+
+	/**
+	 * Metodo que devuelve todos los descuentos
+	 * 
+	 * @return Arraylist de descuentos
+	 */
+	public ArrayList<Descuento> getAll() {
+		ArrayList<Descuento> descuentos = new ArrayList<Descuento>();
+
+		try {
+			Statement statement = conexion.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM descuentos");
+
+			while (resultSet.next()) {
+
+				Descuento descuento = new Descuento();
+				descuento.setId(resultSet.getInt("id"));
+				descuento.setValor(resultSet.getInt("valor"));
+				descuento.setDescripion(resultSet.getString("descripcion"));
+
+				descuentos.add(descuento);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return descuentos;
+
 	}
 
 }
