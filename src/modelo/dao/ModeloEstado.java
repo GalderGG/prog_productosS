03e1fccf72dae03a9abc7c81064c9d83b5495d4c
Galder <Galder@DESKTOP-ALL3S8I>
2020-05-29 22:00:17 +0200

    ModeloEstado aldaketa bisualak

--- src/modelo/dao/ModeloEstado.java
+++ src/modelo/dao/ModeloEstado.java
@@ -1,14 +1,45 @@
 package modelo.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import modelo.Conector;
 import modelo.bean.Estado;
 
-public class ModeloEstado {
-	
-	public ArrayList<Estado> getAll(){
-		//TODO implementar metodo que devuelva todos los estados
-		return null;
-	}
+/**
+ * @author Galder
+ * @version 24.04.2020
+ *
+ */
+public class ModeloEstado extends Conector{
 
+	/**
+	 * Metodo que devuelve todos los estados
+	 * 
+	 * @return Arraylist de estados
+	 */
+	public ArrayList<Estado> getAll() {
+		ArrayList<Estado> estados = new ArrayList<Estado>();
+
+		try {
+			PreparedStatement preparedStatement = conexion.prepareStatement("SELECT * FROM estados");
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				Estado estado = new Estado();
+				estado.setId(resultSet.getInt("id"));
+				estado.setName(resultSet.getString("Name"));
+				estado.setContinent(resultSet.getString("Continent"));
+
+				estados.add(estado);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+		return estados;
+	}
 }
