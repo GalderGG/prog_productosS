5c46f0a3d0a8f53337d8d0d2b291b87d4acf1003
Galder <Galder@DESKTOP-ALL3S8I>
2020-05-29 21:58:55 +0200

    Descuento aldaketa bisualak

--- src/modelo/bean/Descuento.java
+++ src/modelo/bean/Descuento.java
@@ -1,37 +1,72 @@
 package modelo.bean;
 
+/**
+ * @author Galder
+ * @version 24.04.2020
+ *
+ */
 public class Descuento {
 	private int id;
 	private int valor;
-	private String descripion;
+	private String descripcion;
 
+	/**
+	 * Getter
+	 * 
+	 * @return devuelve el atributo id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Setter
+	 * 
+	 * @param id recibe como parametro y lo asingna al atributo      
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return valor devuelve el atributo
+	 */
 	public int getValor() {
 		return valor;
 	}
 
+	/**
+	 * Setter
+	 * 
+	 * @param valor recibe como parametro y lo asingna al atributo           
+	 */
 	public void setValor(int valor) {
 		this.valor = valor;
 	}
 
-	public String getDescripion() {
-		return descripion;
+	/**
+	 * Getter
+	 * 
+	 * @return descripion devuelve el atributo descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
 	}
 
+	/**
+	 * Setter
+	 * 
+	 * @param descripcion recibe como parametro y lo asingna al atributo      
+	 */
 	public void setDescripion(String descripion) {
-		this.descripion = descripion;
+		this.descripcion = descripion;
 	}
 
 	@Override
 	public String toString() {
-		return "Descuento [id=" + id + ", valor=" + valor + ", descripion=" + descripion + "]";
+		return "Descuento [id=" + id + ", valor=" + valor + ", descripion=" + descripcion + "]";
 	}
 
 }
