09a60090c91d7d47916adf30ee34092318d9f86a
Galder <Galder@DESKTOP-ALL3S8I>
2020-05-29 21:57:39 +0200

    Conector aldaketa bisualak

--- src/modelo/Conector.java
+++ src/modelo/Conector.java
@@ -4,30 +4,51 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Esta clase crea la conexion a la base de datos con los datos de config
+ * 
+ * @author Galder
+ * @version 24.04.2020
+ * @see modelo.Config
+ *
+ */
 public class Conector {
-	
+
 	protected Connection conexion;
 
 	/**
+	 * Constructor sin parametros
+	 * 
 	 */
 	protected Conector() {
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-				this.conexion = DriverManager.getConnection("jdbc:mysql://" + Config.HOST + "/" + Config.BBDD, Config.USERNAME, Config.PASSWORD);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			this.conexion = DriverManager.getConnection("jdbc:mysql://" + Config.HOST + "/" + Config.BBDD,
+					Config.USERNAME, Config.PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return devuelve el atributo conexion
+	 */
 	public Connection getConexion() {
 		return conexion;
 	}
 
+	/**
+	 * Setter
+	 * 
+	 * @param conexion recibe como prametro conexion y lo asingna al atributo
+	 */
 	public void setConexion(Connection conexion) {
 		this.conexion = conexion;
 	}
-	
+
 }
