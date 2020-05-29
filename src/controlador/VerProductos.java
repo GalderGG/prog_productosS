6450dc1ef0bea8bcd3f2dc4d7753464902b208af
Galder <Galder@DESKTOP-ALL3S8I>
2020-05-29 22:03:06 +0200

    VerProductos eginda

--- src/controlador/VerProductos.java
+++ src/controlador/VerProductos.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -18,31 +19,38 @@ import modelo.dao.ModeloProducto;
 @WebServlet("/VerProductos")
 public class VerProductos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public VerProductos() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public VerProductos() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		ModeloProducto modeloProducto = new ModeloProducto();
 		ArrayList<Producto> productos = modeloProducto.getAll();
-		
+
 		request.setAttribute("productos", productos);
 		request.getRequestDispatcher("VerProductos.jsp").forward(request, response);
+		
+		try {
+			modeloProducto.getConexion().close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		doGet(request, response);
 	}
 
