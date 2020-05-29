e2dcd2237dc36d47f8f600db21e3d996f19b6456
Galder <Galder@DESKTOP-ALL3S8I>
2020-05-29 21:57:44 +0200

    config aldaketa bisualak

--- src/modelo/Config.java
+++ src/modelo/Config.java
@@ -1,6 +1,13 @@
 package modelo;
 
 public interface Config {
+	/**
+	 * Esta interfaz contiene los datos de la base de datos
+	 * config
+	 * 
+	 * @author Galder
+	 * @version 24.04.2020
+	 */
 	public static final String BBDD = "ud6_productos";
 	public static final String HOST = "localhost";
 	public static final String USERNAME = "root";
